ca7dfeeee0fc1673933a5511908df30221612f47
ajayux <ajay@intersmart.in>
2026-07-01 14:28:02 +0530

    deploy changes

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -8,7 +8,7 @@ const config: Core.Config.Middlewares = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000', process.env.FRONTEND_URL].filter(Boolean),
+      origin: ['https://vitaarah.vercel.app', 'http://localhost:3000'],
     },
   },
 
--- frontend/src/components/layout/floating-action-buttons.jsx
+++ frontend/src/components/layout/floating-action-buttons.jsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import Image from "next/image";
 import ConsultationForm from "../form/consultation-form";
 
@@ -38,13 +37,12 @@ export default function FloatingActionButtons() {
       </a>
 
       <ConsultationForm>
-        <Link
-          href="/contact"
+        <div
           style={{ writingMode: "sideways-lr" }}
           className="text-[12px] lg:text-[11.3px] xl:text-[14px] 2xl:text-[15.8px] 3xl:text-[19.2px] leading-none font-helvetica text-white w-[28px] xl:w-[32px] 2xl:w-[36px] 3xl:w-[44px] h-[85px] xl:h-[105px] 2xl:h-[119px] 3xl:h-[145px] bg-[#a14962] hover:-translate-x-0.5 transition-all duration-300 shadow-md select-none whitespace-nowrap flex items-center justify-center"
         >
           Book Now
-        </Link>
+        </div>
       </ConsultationForm>
     </div>
   );
